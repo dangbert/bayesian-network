3acd0b11ed82651b975722397dbd4bc2d9cd287c
PennyEder <jennz.eder@gmail.com>
2022-12-01 15:00:17 +0100

    added functions

--- BNReasoner.py
+++ BNReasoner.py
@@ -16,3 +16,20 @@ class BNReasoner:
             self.bn = net
 
     # TODO: This is where your methods should go
+    def path_type(self, x, y):
+        """
+        Determines whether a specific node z is a sequence, fork, or collider
+        on the path from x to y.
+        """
+        pass
+
+    def d_blocked(self, x, y):
+        """
+        Checks whether the path between nodes x and y are d-blocked by z.
+        """
+        pass
+
+    def d_seperated(self, x, y):
+        """
+        Checks whether all paths between nodes x and y are d-blocked.
+        """
