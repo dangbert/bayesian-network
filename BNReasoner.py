262b463232f7f6b6c14043ade4c413287262ea6d
PennyEder <jennz.eder@gmail.com>
2022-12-02 19:24:03 +0100

    implement pruning

--- BNReasoner.py
+++ BNReasoner.py
@@ -1,4 +1,4 @@
-from typing import Union
+from typing import Union, MutableSet
 from BayesNet import BayesNet
 
 
@@ -15,12 +15,42 @@ class BNReasoner:
         else:
             self.bn = net
 
-    # TODO: This is where your methods should go
+    def pruning(self, nodes: Tuple(MutableSet[str], MutableSet[str], MutableSet[str])):
+        """
+        Prune network iteratively. Deletes all outgoing edges of nodes in Z. Deletes
+        every leaf node W which is not in sets of nodes X or Y or Z.
+        """
+        X, Y, Z = node_set[0], node_set[1], node_set[2]
+        relevant_nodes = set.union(X, Y, Z)
+
+        nodes = self.bn.get_all_variables()
+
+        # edge pruning
+        for node in Z:
+            children = self.bn.get_children(node)
+            for child in children:
+                self.bn.del_edges([node, child])
+
+        # node pruning
+        while True:
+            leaf_nodes = [node for node in nodes if self.bn.children(node) == []]
+
+            if leaf_nodes = []:
+                break
+
+            for node in leaf_nodes:
+                if node not in relevant_nodes:
+                    self.bn.del_var(node)
+
+
     def path_type(self, x, y):
         """
-        Determines whether a specific node z is a sequence, fork, or collider
+        Determines whether a specific node n is a sequence, fork, or collider
         on the path from x to y.
         """
+        # confirm there is a path between x and y
+        # if nx.is_path(self.bn.structure, [x, y]):
+
         pass
 
     def d_blocked(self, x, y):
